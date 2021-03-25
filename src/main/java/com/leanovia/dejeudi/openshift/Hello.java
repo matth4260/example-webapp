b5f7f0bef7710512943f91e75b6f2bb2f3522db9
Ivan TAMAYO <ivanandre@yahoo.com>
2021-03-25 12:00:50 +0100

    Update Hello.java

--- src/main/java/com/leanovia/dejeudi/openshift/Hello.java
+++ src/main/java/com/leanovia/dejeudi/openshift/Hello.java
@@ -14,6 +14,6 @@ public class Hello {
 @Produces("text/plain")
 public Response index() throws Exception {
     String host = InetAddress.getLocalHost().getHostName();
-    return Response.ok("Hello Leanovia from ...\n" + host).build();
+    return Response.ok("Today is \t " +new java.util.Date() + " Hello Leanovia from ...\n "  + host).build();
   }
 }
