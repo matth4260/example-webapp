32ea1534dc8382110fc0b35a76f0e5754ec46104
Ivan Tamayo <ivan.tamayo@gmail.com>
2021-03-25 11:25:42 +0100

    First commit

---
+++ src/main/java/com/leanovia/dejeudi/openshift/Hello.java
@@ -0,0 +1,19 @@
+package com.leanovia.dejeudi.openshift;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import java.net.InetAddress;
+
+
+@Path("/")
+public class Hello {
+
+@GET
+@Produces("text/plain")
+public Response index() throws Exception {
+    String host = InetAddress.getLocalHost().getHostName();
+    return Response.ok("Veni, vidi, vici...\n" + host).build();
+  }
+}
