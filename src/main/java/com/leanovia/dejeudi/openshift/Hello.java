678b6b087fbbe275887b36feee15df2026f09699
Ivan Tamayo <ivan.tamayo@gmail.com>
2021-04-08 11:50:25 +0200

    Changed message

--- src/main/java/com/leanovia/dejeudi/openshift/Hello.java
+++ src/main/java/com/leanovia/dejeudi/openshift/Hello.java
@@ -14,6 +14,6 @@ public class Hello {
 @Produces("text/plain")
 public Response index() throws Exception {
     String host = InetAddress.getLocalHost().getHostName();
-    return Response.ok(" Hello Leanovia from ...\n "  + "Host:\t"+ host).build();
+    return Response.ok(" Hello Leanovia from ...\n "  + "Host:\t"+ host + "\n New version for the Leanovia Dejeudi").build();
   }
 }
